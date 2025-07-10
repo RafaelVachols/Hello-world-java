469afc0d7fa7d99871452bb6b9b3ad992267fd57
RafaelVachols <rafaelvachols1@gmail.com>
2025-07-10 16:21:14 -0300

    Add files via upload

---
+++ src/Subtração.java
@@ -0,0 +1,7 @@
+public class Subtração{
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println(10 - 5);
+    }
+}
