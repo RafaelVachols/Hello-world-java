1ad68830cbacc329103fbc07f7e1951b5e94ce54
RafaelVachols <rafaelvachols1@gmail.com>
2025-07-11 17:15:32 -0300

    Add files via upload

---
+++ src/Médiafilme.java
@@ -0,0 +1,14 @@
+public class Médiafilme {
+    public static void main(String[] args) {
+        System.out.println("Filme: Creed: Nascido para lutar ");
+
+        int anoDeLancamento =  2015;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        System.out.println("Sinopse: Adonis Johnson nunca conheceu seu pai, Apollo Creed, que faleceu antes do seu nascimento. Ainda assim, a luta está em seu sangue e ele decide entrar no mundo das competições profissionais de boxe. Após muito insistir, Adonis consegue convencer Rocky Balboa a ser seu treinador. ");
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 7.6;
+
+        double media = (6.5 + 8.0 + 8.5 ) / 3;
+        System.out.println(media);
+    }
+}
