d48d3f54abeda24cc18cd27998e8dae7842ae56b
RafaelVachols <rafaelvachols1@gmail.com>
2025-07-14 17:26:06 -0300

    Add files via upload

---
+++ DesafioPrática.java
@@ -0,0 +1,30 @@
+public class DesafioPrática {
+    public static void main(String[] args) {
+     double media =  (8.5 + 9.8 )/2;
+        System.out.println(media);
+
+      double media2 = 3.5;
+      int sing = (int) media2;
+        System.out.println(sing);
+
+      char letra = 'R';
+        System.out.println(letra);
+        String palavra;
+        palavra = """
+                desafio e prática
+                média do filme
+                """;
+        System.out.println(palavra);
+
+        double precoProduto = 50.5;
+        int quantidade = 200;
+        System.out.println(precoProduto*quantidade);
+
+
+        double valorEmDolares = 4.94;
+        
+
+
+
+    }
+}
