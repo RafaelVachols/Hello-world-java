0a051edd538f923e148fe15af3acc2be165aa075
RafaelVachols <rafaelvachols1@gmail.com>
2025-07-15 17:22:18 -0300

    Add files via upload

---
+++ out/production/primeiro-projeto/Loop.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Loop {
+    public static void main(String[] args) {
+        Scanner Leitura = new Scanner(System.in);
+        double mediaAvaliacao = 0;
+        double nota = 0;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Digite sua avaliação para o filme ");
+            nota = Leitura.nextDouble();
+            mediaAvaliacao += nota;
+
+        }
+        System.out.println("Média de avaliação: " + mediaAvaliacao/3);
+
+    }
+}
